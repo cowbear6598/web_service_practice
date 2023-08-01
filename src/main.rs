81f74117295f1962936cf66158b5e4d43ad2f403
CptSoap <cowbear6598@gmail.com>
2023-08-02 01:04:17 +0800

    :white_check_mark: complete main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    App,
+    HttpServer,
+    web::{Data, scope},
+};
+use web_service_pratice::{
+    frameworks::mongo::mongo_client::mongo_connect,
+    frameworks::web,
+};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let client = mongo_connect().await;
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(client.clone()))
+            .service(
+                scope("/api/v1")
+                    .service(web::user_web::add_user)
+            )
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
