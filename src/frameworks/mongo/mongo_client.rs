4136b9ac83b77c73ea64cd90797b7e9aad141227
CptSoap <cowbear6598@gmail.com>
2023-08-05 00:09:54 +0800

    rebase commit

--- src/frameworks/mongo/mongo_client.rs
+++ src/frameworks/mongo/mongo_client.rs
@@ -1,8 +1,10 @@
+use std::env;
 use mongodb::Client;
-use crate::frameworks::mongo::mongo_constants::DB_URI;
 
 pub async fn mongo_connect() -> Client {
-    let client = Client::with_uri_str(DB_URI)
+    let db_uri = env::var("DB_URI").expect("DB_URI 環境變數不存在");
+
+    let client = Client::with_uri_str(db_uri)
         .await
         .expect("與資料庫連結失敗");
 
--- tests/use_cases_tests.rs
+++ tests/use_cases_tests.rs
@@ -1,4 +1,2 @@
 pub mod use_cases;
 pub mod mocks;
-
-pub use mocks::user_mocks;
