914ab011856907494cbd09cad4e712ffa7272cec
cowbear6598 <cowbear6598@gmail.com>
2024-01-05 14:16:32 +0800

    feat: create new container

--- src/container/container.rs
+++ src/container/container.rs
@@ -1,3 +1,17 @@
-pub struct Container {
+use std::sync::Arc;
 
+use crate::mongo::mongo_client::{mongo_connect, MongoClient};
+
+pub struct Container {
+    pub mongo_client: MongoClient,
+}
+
+pub async fn create_container() -> Arc<Container> {
+    let mongo_client = mongo_connect().await;
+
+    let container = Container {
+        mongo_client,
+    };
+
+    Arc::new(container)
 }
